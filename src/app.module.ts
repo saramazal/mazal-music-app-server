80dffa9e5796294c279b4db2287e5207b55b37ee
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-21 12:03:48 +0300

    adding nestjs mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import {Module} from '@nestjs/common'
 import {TrackModule} from './track/track.module'
-
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost/nest'),
+        TrackModule]
 
 })
 export class AppModule {}
