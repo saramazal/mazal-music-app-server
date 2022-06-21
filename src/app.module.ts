762df7726e8be06c0b7e224b13cce3afaa13bcd5
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-21 11:40:19 +0300

    adding track folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import {Module} from '@nestjs/common'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {TrackModule} from './track/track.module'
+
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    imports: [TrackModule]
 
 })
 export class AppModule {}
---
+++ src/track/track.module.ts
@@ -0,0 +1,11 @@
+import {Module} from '@nestjs/common'
+import { TrackController } from './track.controller';
+import {TrackService} from './track.service'
+
+
+@Module({
+    controllers: [TrackController],
+    providers: [TrackService]
+})
+
+export class TrackModule {}
