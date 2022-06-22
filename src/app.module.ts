acfb68852679d97cb7bc9725212f8140ce0f7c80
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-22 10:40:10 +0300

    adding nest serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import {Module} from '@nestjs/common'
 import {TrackModule} from './track/track.module'
 import {MongooseModule} from '@nestjs/mongoose'
 import {FileModule} from './file/file.module'
+import * as path from 'path'
+import {ServeStaticModule} from '@nestjs/serve-static'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
     MongooseModule.forRoot(
-     
-    ),
+    
     TrackModule,
     FileModule
   ]
